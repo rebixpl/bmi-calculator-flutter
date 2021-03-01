4ec6a7274b4f0eb6aeff3624b7949dad2e153b9a
rebixpl <rebixcraft@gmail.com>
2021-03-01 17:23:55 +0100

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,123 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+  // Color maleCardColor = inactiveCardColor;
+  // Color femaleCardColor = inactiveCardColor;
+
+  // void updateColor(Gender selectedGender) {
+  //   if (selectedGender == Gender.Male) {
+  //     // male card pressed
+  //     if (maleCardColor == inactiveCardColor) {
+  //       maleCardColor = activeCardColor;
+  //       femaleCardColor = inactiveCardColor;
+  //     } else {
+  //       maleCardColor = inactiveCardColor;
+  //     }
+  //   } else if (selectedGender == Gender.Female) {
+  //     // female card pressed
+  //     if (femaleCardColor == inactiveCardColor) {
+  //       femaleCardColor = activeCardColor;
+  //       maleCardColor = inactiveCardColor;
+  //     } else {
+  //       femaleCardColor = inactiveCardColor;
+  //     }
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.Male;
+                      });
+                    },
+                    color: selectedGender == Gender.Male
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
+                    child: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      text: "MALE",
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.Female;
+                      });
+                    },
+                    color: selectedGender == Gender.Female
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
+                    child: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      text: "FEMALE",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: kActiveCardColor,
+              child: Column(
+                children: [
+                  Text(
+                    "HEIGHT",
+                    style: k,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: kActiveCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: kActiveCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: kBottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: kBottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/reusable_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({
+    @required this.color,
+    this.child,
+    this.onTap,
+  });
+
+  final Color color;
+  final Widget child;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: this.child,
+      ),
+    );
+  }
+}
